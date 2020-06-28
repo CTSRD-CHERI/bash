bb93721b5b4cc8548da61aedf73e2153fd7b30c6
Jessica Clarke <jrtc27@jrtc27.com>
2020-06-28 02:29:25 +0100

    unwind_prot.c: Ensure SAVED_VAR doesn't tag clear pointers
    unwind_protect_pointer is a thing that exists and will end up putting
    pointers into this structure. I haven't managed to trip up on this yet,
    but presumably it's lurking (and jdw57_memcpy on MIPS picks up on a lone
    pointer having its tag stripped that goes away with this patch).

--- unwind_prot.c
+++ unwind_prot.c
@@ -55,6 +55,9 @@
 typedef struct {
   char *variable;
   int size;
+#ifdef __CHERI_PURE_CAPABILITY__
+  _Alignas(max_align_t)
+#endif
   char desired_setting[1]; /* actual size is `size' */
 } SAVED_VAR;
 
